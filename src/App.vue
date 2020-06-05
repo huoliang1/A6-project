db7b18a212cca0d484dabcba67653c0968720b33
huo ang <1347850517@qq.com>
2020-06-05 12:44:04 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
 <div>
+    <Header/>
+    <router-view></router-view>
+    <Footer></Footer>
 
 </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
+    name:'App',
+    components:{
+      Header,
+      Footer
+    }
 }
-var a = 100
+
 </script>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,14 @@
+// 路由对象
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
 
+// 生命使用vue插件
+Vue.use(VueRouter)
+
+// 向外暴露路由对象
+export default new VueRouter({
+      mode:'history', //不带#
+      routes
+
+})
